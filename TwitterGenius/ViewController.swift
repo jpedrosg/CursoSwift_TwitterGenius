795e6ac75a5c0810e5aa7768c012c3fabede6be7
João Pedro Giarrante <joaopedrogiarrante15@gmail.com>
2020-12-06 22:53:32 -0300

    setting up twitter API

--- TwitterGenius/ViewController.swift
+++ TwitterGenius/ViewController.swift
@@ -15,10 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
-    let swifter = Swifter(consumerKey: "", consumerSecret: "")
+    
+    let apiKey = "API Key"
+    let apiSecret = "API Secret"
+    let name = "Swifter"
+    let type = "plist"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else { return }
+        guard let swifterDic = NSDictionary(contentsOfFile: path) else { return }
+        guard let swifterKey = swifterDic.value(forKey: apiKey) as? String else { return }
+        guard let swifterSecret = swifterDic.value(forKey: apiSecret) as? String else { return }
+        
+        let swifter = Swifter(consumerKey: swifterKey, consumerSecret: swifterSecret)
+        
+        
+        swifter.searchTweet(using: "@Apple") { (results, metadata) in
+            print(results)
+        } failure: { (error) in
+            print(error.localizedDescription)
+        }
+
         
     }
 
